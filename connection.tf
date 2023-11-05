3b14967ba6bef5875199e210f21370a2d832f068
anirudha_dak <123630876+anirudhadak2@users.noreply.github.com>
2023-11-05 10:20:13 +0530

    Create connection.tf

---
+++ connection.tf
@@ -0,0 +1,15 @@
+terraform {
+   required_providers  {
+     aws = {
+     source = "hashicorp/aws"
+     version = "5.7.0"
+    }
+   }
+  }
+
+provider "aws"  {
+        region = var.myregion
+        access_key = var.myaccesskey 
+        secret_key = var.mysecretkey 
+}
+
